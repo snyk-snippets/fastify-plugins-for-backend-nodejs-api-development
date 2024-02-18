c6dec51310395d0e5b5dd74b78fd1471b4aba146
Liran Tal <liran.tal@gmail.com>
2024-02-18 16:23:58 +0200

    feat: add swagger docs
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- server.js
+++ server.js
@@ -34,6 +34,24 @@ fastify.register(import("@fastify/cors"), {
   methods: ["GET", "POST", "PUT", "DELETE"],
 });
 
+fastify.register(import("@fastify/swagger"), {
+  routePrefix: "/documentation",
+  swagger: {
+    info: {
+      title: "Test swagger",
+      description: "testing the fastify swagger api",
+      version: "0.1.0",
+    },
+    host: "localhost",
+    schemes: ["http"],
+    consumes: ["application/json"],
+    produces: ["application/json"],
+  },
+  exposeRoute: true,
+});
+
+fastify.register(import("@fastify/swagger-ui"));
+
 const helloRoute = async (fastify, options) => {
   fastify.route({
     method: "GET",
